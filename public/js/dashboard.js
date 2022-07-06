94cd695072b350118b501962d71a1726059915a2
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2022-07-06 15:46:18 -0300

    resolving cancel request error

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,7 +4,9 @@ function deleteRequest() {
     const requestId = document.querySelector('.request-view').id
     fetch(`/api/requests/${requestId}`, { method: 'DELETE' })
         .then(res => res.json())
-        .then(({ message }) => console.log(message))
-        .catch(alert)
-    location.reload()
+        .then(({ message }) => {
+            console.log(message)
+            location.reload()
+        })
+        .catch(console.error)
 }
